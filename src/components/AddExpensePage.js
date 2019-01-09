ce7d235c2ef87a48a070c014594f8880220d7ff8
John Cameron <john.n.cameron@gmail.com>
2019-01-09 23:35:38 +1100

    ejecting

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddExpensePage extends Component {
+  render() {
+    return(
+      <div>Add Expense here...</div>
+    )
+  }
+}
+
+export default AddExpensePage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import AddExpensePage from './components/AddExpensePage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={App}/>
+      <Route path="/create" component={AddExpensePage} />
+    </div>
+  </BrowserRouter>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routes, document.getElementById('root'));
+
+
