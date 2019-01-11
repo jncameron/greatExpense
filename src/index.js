4fa817d1c4e274e9c7a762f8768a7dd408d214dd
John Cameron <john.n.cameron@gmail.com>
2019-01-11 12:45:24 +1100

    redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routes/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', note:'for water', amount:1500}));
+
+store.dispatch(setTextFilter('bill'));
 
 
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-
+console.log(visibleExpenses);
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
