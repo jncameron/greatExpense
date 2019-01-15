0d241ab4d31d5e96eee5d9f1eeaae2dd8f37b69d
John Cameron <john.n.cameron@gmail.com>
2019-01-15 21:18:50 +1100

    add and edit expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
+
 
 class AddExpensePage extends Component {
-  render() {
+  render(props) {
     return(
       <div>
         <h1>Add Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm 
+          onSubmit={(expense) => {
+            this.props.dispatch(addExpense(expense));
+            this.props.history.push('/');
+          }}
+        />
       </div>
     )
   }
 }
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
@@ -6,8 +7,10 @@ const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => {
 
     return (
       <div>
+        <Link to={`/edit/${id}`}>
         <h3>Description: {description}</h3>
-        <h4>Amount: ${amount}</h4>
+        </Link>
+        <h4>Amount: {amount}</h4>
         <h5>Created: {createdAt}</h5>
         <button onClick={() => {
           dispatch(removeExpense({id}));
