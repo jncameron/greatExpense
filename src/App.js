dd369cb1ec822b3b821c98e1078eee4429f2c2aa
John Cameron <john.n.cameron@gmail.com>
2019-01-18 09:29:46 +1100

    expenses total

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ExpenseList from './components/ExpenseList';
 import ExpenseListFilters from './components/ExpenseListFilters';
+import ExpensesSummary from './components/ExpensesSummary';
 import './styles/styles.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
       </div>
