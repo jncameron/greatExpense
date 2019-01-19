9ba709db32f95aa1cc211f385070b63d429d2e50
John Cameron <john.n.cameron@gmail.com>
2019-01-19 16:22:04 +1100

    style header and buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 const LoginPage = ({startLogin}) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div class="box-layout__box">
+      <h1 class="box-layout__title">Great Expense</h1>
+      <p>Track your spending</p>
+      <button className="button" onClick={startLogin}>Login with Google</button>
+    </div>
   </div>
 );
 
